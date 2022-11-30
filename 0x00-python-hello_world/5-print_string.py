f74fc2acfc9882f2f797917c7320cf2b930f093f
Databoy69 <potuya8@gmail.com>
2022-11-30 07:01:47 +0000

    python3

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Holberton school"
-print(3 * str)
+print(str*3)
 print(str[:9])
--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,4 +1,4 @@
-#!/urs/python3
+#!/urs/bin/python3
 word = "Holberton"
 word_first_3 = word[:3]
 word_last_2 = word[-2:]
