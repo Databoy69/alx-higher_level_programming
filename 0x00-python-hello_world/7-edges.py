849f03c455ce7f5cd52a89dbb57a9bf209523f0c
Databoy69 <potuya8@gmail.com>
2022-11-29 20:52:53 -0800

    learning to program in python 0x00 hello world

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/urs/python3
+word = "Holberton"
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
+print("first 3 letters: {}".format(word_first_3))
+print("last 2 letters: {}".format(word_last_2))
+print("middle word: {}".format(middle_word))
