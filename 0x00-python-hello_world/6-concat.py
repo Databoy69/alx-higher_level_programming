defea001fbdb74b6eac4a4ccb9e118c129788068
Databoy69 <potuya8@gmail.com>
2022-12-01 02:31:14 +0000

    Play with strings

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
-str2 = "school"
-str1 = str1  + " " + str2
-print("weclome to {}!".format(str1))
-
+str2 = "School"
+str1 = str1 + " " + str2
+print("Welcome to {}!".format(str1))
