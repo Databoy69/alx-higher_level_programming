27fe3cef4ae35ca2d7f8e9597c7361cd524681e2
Databoy69 <potuya8@gmail.com>
2022-12-09 18:38:16 -0800

    number_keys

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-number_keys = __import__('5-number_keys').number_keys
+def number_keys(a_dictionary):
+    num = 0
+    list_keys = list(a_dictionary.keys())
 
-a_dictionary = { 'language': "C", 'number': 13, 'track': "Low level" }
-nb_keys = number_keys(a_dictionary)
-print("Number of keys: {:d}".format(nb_keys))
+    for i in list_keys:
+        num += 1
+
+    return (num)
