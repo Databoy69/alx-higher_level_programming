297224b4bf74e521fcfd78c0e944a50047c899ac
Databoy69 <potuya8@gmail.com>
2023-03-15 13:30:09 -0700

    0x12-javascript-warm_up

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (process.argv.length === 2) {
+	console.log('No Argument found');
+} else if (process.argv.lenght === 3) {
+	console.log('Argument found');
+} else {
+	console.log('Argument found');
+}
---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,2 @@
+#!/usr/bi/node
+consloe.log(process.argv[2] + 'is' + process.argv[3]);
